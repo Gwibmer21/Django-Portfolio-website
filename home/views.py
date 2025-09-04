f223042bc6b8f83c266e136098388fd3de71d3d1
Gerardo <gwibmer@usf.edu>
2025-09-04 08:02:50 -0400

    latest updates

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact
 
 # Create your views here.
 
@@ -23,14 +24,29 @@ def user_management_dashboard(request):
     return render(request, 'user_management_dashboard.html')
 
 
+def ancap_automation(request):
+    return render(request, 'ancap_automation.html')
+
+
 def contact(request):
     #contact form database
     if request.method == "POST":
-        name == request.POST['name']
-        email == request.POST['email']
-        subject == request.POST['subject']
-        message == request.POST['message']
-        contact = models.Home(name=name, email=email, subject=subject, message=message)
-        contact.save()
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        subject = request.POST.get('subject', '')
+        message = request.POST.get('message', '')
+        Contact.objects.create(name=name, email=email, subject=subject, message=message)
     return render(request, 'home.html')
+
+
+def reroom(request):
+    return render(request, 'reroom.html')
+
+
+def insurance_call_simulator(request):
+    return render(request, 'insurance_call_simulator.html')
+
+
+def ocr_pdf_extractor(request):
+    return render(request, 'ocr_pdf_extractor.html')
 
